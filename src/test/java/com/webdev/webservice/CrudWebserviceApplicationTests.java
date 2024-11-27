644faa44739d9490a4e524494ff50b50885286d7
Sultan <osultan@yandex.ru>
2024-11-27 20:31:25 +0500

    Initial commit

---
+++ src/test/java/com/webdev/webservice/CrudWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webdev.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudWebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
