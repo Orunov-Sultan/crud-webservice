9fd953fa79a8e8deff07a41ad5363fae9b9fc0cf
Sultan <osultan@yandex.ru>
2024-11-27 21:58:28 +0500

    Обработал исключение когда в id передают строку в место числа

--- src/main/java/com/webdev/webservice/exception/UserExceptionHandler.java
+++ src/main/java/com/webdev/webservice/exception/UserExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.time.LocalDateTime;
 
@@ -19,5 +20,15 @@ public class UserExceptionHandler {
         error.setStatusCode(HttpStatus.NOT_FOUND.value());
         error.setPath(request.getDescription(false));
         return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorDetails> MethodArgumentTypeMismatch(MethodArgumentTypeMismatchException exception, WebRequest request) {
+        ErrorDetails error = new ErrorDetails();
+        error.setTimestamp(LocalDateTime.now());
+        error.setMessage(exception.getMessage());
+        error.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        error.setPath(request.getDescription(false));
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
 }
