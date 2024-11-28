dd536050e2d5e6896dc48595c703b345bca61cec
Sultan <osultan@yandex.ru>
2024-11-28 09:44:00 +0500

    Добавил документацию API

--- src/main/java/com/webdev/webservice/CrudWebserviceApplication.java
+++ src/main/java/com/webdev/webservice/CrudWebserviceApplication.java
@@ -1,11 +1,26 @@
 package com.webdev.webservice;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Документация Spring Boot Rest API",
+                description = "Документация Spring Boot Rest API",
+                version = "v1.0",
+                contact = @Contact(
+                        name = "Sultan",
+                        email = "osultan@yandex.ru",
+                        url = "https://github.com/Orunov-Sultan"
+                )
+        )
+)
 public class CrudWebserviceApplication {
     @Bean
     public ModelMapper modelMapper(){
--- src/main/java/com/webdev/webservice/controller/UserController.java
+++ src/main/java/com/webdev/webservice/controller/UserController.java
@@ -3,6 +3,9 @@ package com.webdev.webservice.controller;
 import com.webdev.webservice.dto.UserDTO;
 import com.webdev.webservice.entity.User;
 import com.webdev.webservice.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,33 +17,77 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/users")
 @AllArgsConstructor
+@Tag(
+        name = "CRUD Rest API для пользователя",
+        description = "Создать польователя, Обновить Пользователя, Найти Пользователя, Найти всех Пользователей, Удалить Пользователя"
+)
 public class UserController {
     private UserService userService;
 
+    @Operation(
+            summary = "Создание пользователя Rest API",
+            description = "Сохранение данных о пользователе в базе данных"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Http Status 201 CREATED пользователь создан"
+    )
     @PostMapping
     public ResponseEntity<UserDTO> createUser(@RequestBody @Valid UserDTO userDTO) {
         UserDTO createdUser = userService.createUser(userDTO);
         return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Получение пользователя Rest API",
+            description = "Получение пользователя по Id из базы данных"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 OK операция прошла успешно"
+    )
     @GetMapping("{id}")
     public ResponseEntity<UserDTO> findUserById(@PathVariable Long id) {
         UserDTO userDTO = userService.findUserById(id);
         return ResponseEntity.ok(userDTO);
     }
 
+    @Operation(
+            summary = "Получение всез пользователей Rest API",
+            description = "Получение всех пользователей из базы данных"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 OK операция прошла успешно"
+    )
     @GetMapping
     public ResponseEntity<List<UserDTO>> findAllUsers() {
         List<UserDTO> usersDto = userService.findAllUsers();
         return ResponseEntity.ok(usersDto);
     }
 
+    @Operation(
+            summary = "Обновление пользователя Rest API",
+            description = "Обновление пользователя из базы данных"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 OK операция прошла успешно"
+    )
     @PutMapping("{id}")
     public ResponseEntity<UserDTO> updateUser(@PathVariable Long id, @RequestBody @Valid UserDTO userDTO) {
         UserDTO updatedUser = userService.updateUser(id, userDTO);
         return ResponseEntity.ok(updatedUser);
     }
 
+    @Operation(
+            summary = "Удаление пользователя Rest API",
+            description = "Удаление пользователя по Id из базы данных"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 OK операция прошла успешно"
+    )
     @DeleteMapping("{id}")
     public ResponseEntity<String> deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
